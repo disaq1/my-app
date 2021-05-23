90369910a43716e424120a2730cc5afe8b331255
disaq1 <slava79198015981@yandex.ru>
2021-05-23 21:53:50 +0400

    Adds the initial markup of the 'Settings' page

---
+++ src/components/Settingss/Settingss.jsx
@@ -0,0 +1,11 @@
+﻿import './Settingss.css'
+
+const Settingss = (props) => {
+    return (
+        <div className='dialogs'>
+            Settingss
+        </div>
+    )
+}
+
+export default Settingss;
