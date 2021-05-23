ecb4853c96c0a956566320c800475b8eaef42359
disaq1 <slava79198015981@yandex.ru>
2021-05-24 01:10:48 +0400

    Переписывает структуру для автоматической генерации постов при появлении новых позиций в массиве

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,14 @@ import Post from './Post/Post'
 
 const MyPosts = () => {
 
-    let postsData = [
-        {id:1, message: 'My first post', likesCount: 1},
-        {id:2, message: 'Slava, wat a u doing???', likesCount: 4},
-        {id:3, message: 'And right now я пишу на великом и могучем', likesCount: 8},
-        {id:4, message: 'WOAW я тоже могу на русском писать!!! Слава, что ты сделал???', likesCount: 8},
+    let posts = [
+        {id:1, name: 'Tanya', message: 'My first post', likesCount: 100},
+        {id:2, name: 'Anya', message: 'Slava, wat a u doing???', likesCount: 4},
+        {id:3, name: 'Manya', message: 'And right now я пишу на великом и могучем', likesCount: 8},
+        {id:4, name: 'Sanya', message: 'WOAW я тоже могу на русском писать!!! Слава, что ты сделал???', likesCount: 8},
     ]
 
-    let authorsData = [
-        {id:1, name: 'Tanya'},
-        {id:2, name: 'Polina'},
-        {id:3, name: 'Vlad'},
-        {id:4, name: 'Max'},
-    ]
+    let postsElements = posts.map ( post => <Post name={post.name}  message={post.message} likesCount={post.likesCount}/> )
 
     return (
             <div className="profile__posts">
@@ -32,11 +27,7 @@ const MyPosts = () => {
                     <span>
                         All posts
                     </span>
-
-                    <Post name={authorsData[0].name} message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                    <Post name={authorsData[1].name} message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-                    <Post name={authorsData[2].name} message={postsData[2].message} likesCount={postsData[2].likesCount}/>
-                    <Post name={authorsData[3].name} message={postsData[3].message} likesCount={postsData[3].likesCount}/>
+                    {postsElements}
                 </div>
 
             </div>
