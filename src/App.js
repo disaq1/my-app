eee069a31e21d44c770bcc9f4e01c014721c85eb
disaq1 <slava79198015981@yandex.ru>
2021-05-23 21:48:05 +0400

    Adds Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 import Header from '../src/components/Header/Header';
 import MainProfile from '../src/components/Profile/MainProfile';
 import Navbar from '../src/components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route } from 'react-router-dom'
+import Friends from './components/Friends/Firends';
+import Groups from './components/Groups/Groups';
+import Photos from './components/Photos/Photos';
+import Videos from './components/Videoss/Videos';
+import Novosti from './components/Novosti/Novosti';
+import Settingss from './components/Settingss/Settingss';
 
 
 
@@ -16,10 +22,14 @@ const App = () => {
                 <Header />
                 <Navbar />
                 <div className="main">
-                    <Route />
-                    <Route />
-                    {/* <Dialogs /> */}
-                    <MainProfile />
+                    <Route  path='/dialogs' component={Dialogs}/>
+                    <Route  path='/profile' component={MainProfile}/>
+                    <Route  path='/friends' component={Friends}/>
+                    <Route  path='/groups' component={Groups}/>
+                    <Route  path='/photos' component={Photos}/>
+                    <Route  path='/videos' component={Videos}/>
+                    <Route  path='/news' component={Novosti}/>
+                    <Route  path='/settings' component={Settingss}/>
                 </div>
             </div>
         </BrowserRouter>
