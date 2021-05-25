29928830c0d50bcdf386d763bb3eec67739c1359
disaq1 <slava79198015981@yandex.ru>
2021-05-25 14:37:15 +0400

    Прописывает разметку карточки Friend

---
+++ src/components/Friends/Friend/Friend.jsx
@@ -0,0 +1,25 @@
+﻿import s from './Friend.module.css'
+
+const Friend = (props) => {
+
+    return (
+        <div className={s.friend}>
+            <img src={props.img} />
+            <div className={s.description}>
+                <span className={s.name}>
+                    {props.name} {props.surname}
+                </span>
+                <span className={s.status}>
+                    {props.status}
+                </span>
+            </div>
+            <div className={s.btns}>
+                <button className={s.btn}>Write message</button>
+                <button className={s.btn}>Edit friend lists</button>
+                <button className={s.btn}>Unfriend</button>
+            </div>
+        </div>
+    )
+}
+
+export default Friend;
