a41d459bda0ea702b7a0808070d336b58e39ece1
disaq1 <slava79198015981@yandex.ru>
2021-05-25 14:28:19 +0400

    1)Убирает массисы из файла и переносит в БД 2)Удаляет не используемую функцию

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,25 +4,8 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id:1, name: 'Tanya'},
-        {id:2, name: 'Polina'},
-        {id:3, name: 'Vlad'},
-        {id:4, name: 'Max'},
-    ]
-
-    let messages = [
-        {id:1, message: 'Это первые сообщения на русском'},
-        {id:2, message: 'Вау, до чего дошёл прогресс'},
-        {id:3, message: 'Это ещё что, то ли ещё будет'},
-        {id:4, message: 'Вау, какой я гений...<3'},
-    ]
-
-    let dialogElements = dialogs.map ( dialog => <Dialog name={dialog.name} id={dialog.id}/> )
-
-    let messageElements = messages.map ( message => <Message message={message.message}/> )
-
-
+    let dialogElements = props.dialogs.map ( dialog => <Dialog name={dialog.name} id={dialog.id}/> )
+    let messageElements = props.messages.map ( message => <Message message={message.message}/> )
 
     return (
         <div className={s.dialogs}>
@@ -58,15 +41,5 @@ const Message = (props) => {
         </div>
     )
 }
-
-const YourMessage = (props) => {
-    return (
-        <div className={s.message + ' ' + s.yourMessage}>
-        <img src="https://thumbs.dreamstime.com/z/default-avatar-profile-icon-grey-photo-placeholder-male-default-profile-gray-person-picture-isolated-white-background-good-man-102846161.jpg" />
-        {props.message}
-    </div>
-    )
-}
-
 
 export default Dialogs;
