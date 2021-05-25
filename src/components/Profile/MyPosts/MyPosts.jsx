4998ef790dd2aaa92f2eb176d4c8a861e83d169f
disaq1 <slava79198015981@yandex.ru>
2021-05-25 14:36:41 +0400

    1)Прокидывает props на страницу 2)Переносит массив с информацией о каждом посте в БД

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,9 @@
 ﻿import './MyPosts.css'
 import Post from './Post/Post'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id:1, name: 'Tanya', message: 'My first post', likesCount: 100},
-        {id:2, name: 'Anya', message: 'Slava, wat a u doing???', likesCount: 4},
-        {id:3, name: 'Manya', message: 'And right now я пишу на великом и могучем', likesCount: 8},
-        {id:4, name: 'Sanya', message: 'WOAW я тоже могу на русском писать!!! Слава, что ты сделал???', likesCount: 8},
-    ]
-
-    let postsElements = posts.map ( post => <Post name={post.name}  message={post.message} likesCount={post.likesCount}/> )
+    let postsElements = props.posts.map ( post => <Post name={post.name}  message={post.message} likesCount={post.likesCount}/> )
 
     return (
             <div className="profile__posts">
