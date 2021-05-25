b4178abc6109f20741432482789f696773fc9404
disaq1 <slava79198015981@yandex.ru>
2021-05-25 14:33:36 +0400

    Прописывает разметку страницы Friends

---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,27 @@
+﻿import Friend from './Friend/Friend'
+import s from './Friends.module.css'
+
+const Friends = (props) => {
+
+    let friends = props.friendsInfo.map ( 
+                                        friendInfo => 
+                                            <Friend 
+                                                name={friendInfo.name} 
+                                                surname={friendInfo.surname} 
+                                                status={friendInfo.status} 
+                                                img={friendInfo.img}
+                                            /> 
+                                        )
+
+    return (
+        <div className={s.friends}>
+            <div className={s.search}>
+                <input type="search" placeholder="Start searching in Friends"/>
+            </div>
+            {friends}
+        </div>
+    )
+}
+
+
+export default Friends;
