bdbd9eb4368782d347d79aa1a8c310eb522ec7e1
disaq1 <slava79198015981@yandex.ru>
2021-06-15 05:09:32 +0400

    Обновил структуру разметки навигационного меню сайта

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,50 +3,32 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <ul className="nav__items">
-                <NavLink className="nav__link" to='/profile'>
-                    <li className="nav__item">
-                        Profile
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/dialogs'>
-                    <li className="nav__item">
-                        Dialogs
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/friends'>
-                    <li className="nav__item">
-                        Friends
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/groups'>
-                    <li className="nav__item">
-                        Groups
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/photos'>
-                    <li className="nav__item">
-                        Photos
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/videos'>
-                    <li className="nav__item">
-                        Videos
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/news'>
-                    <li className="nav__item">
-                        News
-                    </li>
-                </NavLink>
-                <NavLink className="nav__link" to='/settings'>
-                    <li className="nav__item">
-                        Settings
-                    </li>
-                </NavLink>
-            </ul>
-        </nav>
+        <div className="nav">
+            <NavLink className="nav__link" to='/profile'>
+                Profile
+            </NavLink>
+            <NavLink className="nav__link" to='/dialogs'>
+                Dialogs
+            </NavLink>
+            <NavLink className="nav__link" to='/friends'>
+                Friends
+            </NavLink>
+            <NavLink className="nav__link" to='/groups'>
+                Groups
+            </NavLink>
+            <NavLink className="nav__link" to='/photos'>
+                Photos
+            </NavLink>
+            <NavLink className="nav__link" to='/videos'>
+                Videos
+            </NavLink>
+            <NavLink className="nav__link" to='/news'>
+                News
+            </NavLink>
+            <NavLink className="nav__link" to='/settings'>
+                Settings
+            </NavLink>
+        </div>
     )
 }
 
