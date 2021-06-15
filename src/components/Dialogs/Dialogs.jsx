29fe699ccd748a2cfa52ae65c864adf5f0dcde79
disaq1 <slava79198015981@yandex.ru>
2021-06-15 05:15:36 +0400

    Добавил первоначальный вид функций обработки value в textarea Обновил разметку страницы "Диалоги" для соответствия макету

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,50 @@
-﻿import { NavLink } from 'react-router-dom'
+﻿import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 
 const Dialogs = (props) => {
 
+    // let dialogName = props.dialogs.map ( dialog => <DialogName name={dialog.name} /> )
     let dialogElements = props.dialogs.map ( dialog => <Dialog name={dialog.name} id={dialog.id}/> )
     let messageElements = props.messages.map ( message => <Message message={message.message}/> )
 
+
+    let newMessage = React.createRef();
+    let sendMessage = () => {
+        let text = newMessage.current.value;
+        alert(text);
+    }
+
     return (
         <div className={s.dialogs}>
-            <div className={s.name}>Dialogs</div>
-            <div className={s.search}><input type="search" placeholder="Start searching in Dialogs"/></div>
-            <div className={s.conference}>New&nbsp;dialog</div>
+                <div className={s.thisPage}>
+                    Dialogs
+                </div>
+                <div className={s.extraMenu}>
+                    <button>Help</button>
+                    <button>Exit</button>
+                </div>
             <div className={s.dialogsItems}>
+                <div className={s.search}><input type="search" placeholder="Start searching in Dialogs"/></div>
                 {dialogElements}
             </div>
             <div className={s.dialogsMessage}>
+                {/* {dialogName} */}
+                <span>Name Surname</span>
                 {messageElements}
+                <textarea ref={ newMessage } cols="49" rows="2"></textarea>
+                <button onClick={ sendMessage }>Send message</button>
             </div>
         </div>
     )
 }
+
+// const DialogName = (props) => {
+//     return (
+//             <span>{props.name}</span>
+//     )
+// }
 
 const Dialog = (props) => {
     return (
